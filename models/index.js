131e2ccbe2ff9f044c732e0f3662f5c0dce73bd7
Matthew Baty <mattbaty@outlook.com>
2023-05-12 20:15:41 -0400

    models/index.js done, models/User.js done

--- models/index.js
+++ models/index.js
@@ -9,3 +9,25 @@ User.hasMany(Post, {
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {
+    User,
+    Post,
+    Comment
+}
