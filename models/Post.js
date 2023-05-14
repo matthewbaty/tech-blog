9bced6cbc7ce8a21a7e0220ba96643511f9c30aa
Matthew Baty <mattbaty@outlook.com>
2023-05-14 17:42:00 -0400

    models/comment.js done

--- models/Post.js
+++ models/Post.js
@@ -32,7 +32,7 @@ Post.init(
     },
     {
         sequelize,
-        timestamps: false,
+        timestamps: true,
         freezeTableName: true,
         underscored: true,
         modelName: 'post'
