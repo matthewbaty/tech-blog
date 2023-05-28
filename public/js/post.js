893f82e3e565a4ffd45376921f68e8fa359a44ff
Matthew Baty <mattbaty@outlook.com>
2023-05-28 19:54:41 -0400

    post.js halfway done

---
+++ public/js/post.js
@@ -0,0 +1,72 @@
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+
+    if (title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Post Creation Failed')
+        }
+    }
+};
+
+const postId = window.location.toString().split('/')
+[
+    window.location.toString().split('/').length - 1
+];
+
+const updatePostFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#update-post-title').value.trim();
+    const content = document.querySelector('#update-post-content').value.trim();
+
+    if (title && content) {
+        const response = await fetch(`/api/posts/${postId}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Post Update Failed')
+        }
+    }
+};
+
+const deletePostFormHandler = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`/api/posts/${postId}`, {method:'DELETE'});
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    }   else {
+        alert('Post Delete Failed');
+    }
+};
+
+const deletePost = async (postId) => {
+    const response = await fetch(`/api/posts/${postId}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert('Post Delete Failed');
+    }
+};
+
